9f8e3a8880068889055cb4159cc92bcdaf0bbd32
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2024-07-04 15:06:08 +0700

    Update p.sh

--- p.sh
+++ p.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
+# Check if the script is run as the user who wants to change the password
+if [ "$(whoami)" != "$USER" ]; then
+   echo "This script must be run as the user whose password is being changed" 1>&2
    exit 1
 fi
 
-# Prompt for the username whose password you want to reset
-read -p "Enter the username whose password you want to reset: " USERNAME
-
-# Check if the user exists
-if id "$USERNAME" &>/dev/null; then
-    # Reset the password
-    echo "$USERNAME:nekoneko12" | chpasswd
-    echo "Password for user $USERNAME has been reset to 'nekoneko12'."
-else
-    echo "User $USERNAME does not exist."
-fi
+# Change the password for the current user
+echo "$USER:nekoneko12" | chpasswd
+echo "Password for user $USER has been reset to 'nekoneko12'."
